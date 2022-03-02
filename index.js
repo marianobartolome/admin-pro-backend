478053a4e506426e9b0145bdebeeb078d5c0fd2c
mariano <marianob@itcsoluciones.com>
2022-03-02 17:25:15 -0300

    SPA control

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 //import express from 'express';
 const express= require('express');
@@ -30,6 +31,11 @@ app.use('/api/medicos', require ('./routes/medicos'));
 app.use('/api/login', require ('./routes/auth'));
 app.use('/api/todo', require ('./routes/busquedas'));
 app.use('/api/upload', require ('./routes/uploads'));
+
+//lo ultimo
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'public/index.html'));
+});
 
 
 app.listen(process.env.PORT, () => {
